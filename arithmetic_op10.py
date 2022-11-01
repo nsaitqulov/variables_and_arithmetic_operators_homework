e38402c4fddfb9317b5705499521ad577cfdc93c
Nurbek <nurbek.saitqulov@gmail.com>
2022-11-01 15:55:04 +0500

    10

--- arithmetic_op10.py
+++ arithmetic_op10.py
@@ -1,5 +1,6 @@
 # Create a variable "n"  and assign it the value of the 4.
-
+n=4
 # Create a variable called 'answer'.
-
+answer=2**n
 # Find the expression value assign to 'answer'. 2^n.
+print(answer)
